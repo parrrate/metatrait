59f38846946c72bd81bec16616f4e10a98480234
parrrate <alisa@parrrate.com>
2025-09-12 18:05:56 +0000

    impl Select for Futures

--- src/linked/functional/instances/futures.rs
+++ src/linked/functional/instances/futures.rs
@@ -1,5 +1,8 @@
+use either::Either;
+use futures::future::select;
+
 use crate::linked::{
-    functional::{Map, Map2, MapFn, MapFn2, Pure, Wrap},
+    functional::{Flatten, Map, Map2, MapFn, MapFn2, Pure, Select, SelectFn, Union, UnionFn, Wrap},
     traits::future::{ToFuture, ToFutureExt},
     Impl, Trait,
 };
@@ -35,5 +38,43 @@ impl Map2 for Futures {
             let (x0, x1) = futures::join!(x0.to_future(), x1.to_future());
             f.run(x0, x1)
         }
+    }
+}
+
+impl Union for Futures {
+    fn union<F: UnionFn>(
+        x: Either<impl Impl<Self::Wrap<F::Out>>, impl Impl<Self::Wrap<F::Out>>>,
+    ) -> impl Impl<Self::Wrap<F::Out>> {
+        async {
+            F::union(match x {
+                Either::Left(x) => Either::Left(x.to_future().await),
+                Either::Right(x) => Either::Right(x.to_future().await),
+            })
+        }
+    }
+}
+
+impl Select for Futures {
+    fn select<In0: ?Sized + Trait, In1: ?Sized + Trait, F: SelectFn<Self, In0, In1>>(
+        x0: impl Impl<Self::Wrap<In0>>,
+        x1: impl Impl<Self::Wrap<In1>>,
+        f: F,
+    ) -> impl Impl<Self::Wrap<F::Out>> {
+        async {
+            F::union(
+                match select(Box::pin(x0.to_future()), Box::pin(x1.to_future())).await {
+                    futures::future::Either::Left((x, y)) => Either::Left(f.run0(x, y)),
+                    futures::future::Either::Right((x, y)) => Either::Right(f.run1(x, y)),
+                },
+            )
+        }
+    }
+}
+
+impl Flatten for Futures {
+    fn flatten<Tr: ?Sized + Trait>(
+        x: impl Impl<Self::Wrap<Self::Wrap<Tr>>>,
+    ) -> impl Impl<Self::Wrap<Tr>> {
+        async { x.to_future().await.to_future().await }
     }
 }
